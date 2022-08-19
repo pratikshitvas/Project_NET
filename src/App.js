f2b0524d1404c7775b6ccb91688e5584b3fe1bc3
pratikshit vashistha <50011440+pratikshitvas@users.noreply.github.com>
2022-08-19 17:46:09 +0530

    stil login

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { AuthContextProvider } from "./context/AuthContext";
 import Login from "./Pages/Login";
 import Signup from "./Pages/Signup";
 import Account from "./Pages/Account";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
       <Route path='/' element={<Home/>}/>
       <Route path="/login" element={<Login/>}/> 
       <Route path="/signUp" element={<Signup />}/> 
-      <Route path="/account" element={<Account />}/> 
+      <Route path="/account" element={<ProtectedRoute><Account /></ProtectedRoute>}/> 
     </Routes>
     </AuthContextProvider>
     </>
